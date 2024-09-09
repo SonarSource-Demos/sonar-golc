3e5baf08cf568e27afc99bb52c25798c0ebcfe83
Cole Gannaway <156719330+cole-gannaway-sonarsource@users.noreply.github.com>
2024-09-09 13:52:41 -0500

    added a test for linux-arm64 platform to be scripted

---
+++ tests/test-linux-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run the Docker container
+docker build -t sonar-golc-linux-arm64-test -f Dockerfile.test-linux-arm64 . && \
+docker run --rm sonar-golc-linux-arm64-test
+
+# Capture the exit code
+EXIT_CODE=$?
+
+# Check the exit code
+if [ $EXIT_CODE -eq 0 ]; then
+  echo "Test passed."
+else
+  echo "Test failed with exit code $EXIT_CODE."
+fi
+
+# Exit with the same code as the Docker container
+exit $EXIT_CODE
